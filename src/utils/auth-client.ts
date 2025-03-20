ccb8f19ff46c72b6fdee171c095124f6ffff506c
noahgerard <helix.dev.xyz@gmail.com>
2025-03-20 10:20:03 -0400

    feat: use username sign-on method

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,7 +1,9 @@
+import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react"
 
-export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000" // the base url of your auth server
+export const { signIn, signUp, useSession } = createAuthClient({
+    baseURL: "http://localhost:3000", // the base url of your auth server
+    plugins: [
+        usernameClient()
+    ]
 });
-
-export const { signIn, signUp, useSession } = createAuthClient();
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,36 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
- 
+import { username } from "better-auth/plugins";
+
 const prisma = new PrismaClient();
 export const auth = betterAuth({
+    plugins:
+        [
+            username({
+                minUsernameLength: 5,
+                maxUsernameLength: 20,
+                usernameValidator: (username) => {
+                    if (username === "admin") {
+                        return false
+                    }
+
+                    // Usernames must be alphanumeric and can contain underscores
+                    const regex = /^[a-zA-Z0-9_]+$/;
+                    if (!regex.test(username)) {
+                        return false
+                    }
+
+                    // Usernames must not contain spaces
+                    if (/\s/.test(username)) {
+                        return false
+                    }
+
+                    return true
+                }
+            })
+        ]
+    ,
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
