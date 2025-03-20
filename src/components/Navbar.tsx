59bf46fc0aee293023f27bbc44ac20f7d5482609
noahgerard <helix.dev.xyz@gmail.com>
2025-03-20 12:15:34 -0400

    feat: Added shadcn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 
 export function Navbar() {
-	return <div className="absolute flex justify-center w-full border-2">
+	return <div className="absolute flex justify-center w-full bg-slate-100">
 		<div className="container flex justify-between p-8">
 			<p>StockDash</p>
 			<p>Get Started</p>
