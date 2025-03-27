05d8a160bc3e85fd6c7e4ab1ff749dd58bcebd66
noahgerard <helix.dev.xyz@gmail.com>
2025-03-27 16:49:51 -0400

    feat: Portfolio page now fetches data

--- src/app/(protected)/dashboard/portfolios/page.tsx
+++ src/app/(protected)/dashboard/portfolios/page.tsx
@@ -3,12 +3,13 @@
 import { useState } from "react";
 import { api } from "~/trpc/react";
 
+// TODO: Add pagination functionality
+
 import {
   Table,
   TableBody,
   TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -25,7 +26,7 @@ import {
 } from "~/components/ui/pagination";
 
 import { Button } from "~/components/ui/button";
-import { PlusIcon, Settings } from "lucide-react";
+import { Settings } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -34,30 +35,12 @@ import {
 } from "~/components/ui/card";
 import NewPortButton from "~/components/NewPortButton";
 
-const portfolios: {
-  id: number;
-  name: string;
-  dailyChange: string;
-  cashBalance: string;
-  totalValue: string;
-}[] = [];
-
-for (let i = 0; i < Math.random() * 20 + 10; i++) {
-  portfolios.push({
-    id: i,
-    name: `Portfolio ${i}`,
-    dailyChange: `${(Math.random() * 10 - 5).toFixed(2)}%`,
-    cashBalance: `$${(Math.random() * 10000).toFixed(2)}`,
-    totalValue: `$${(Math.random() * 100000).toFixed(2)}`,
-  });
-}
-
 export default function Dashboard() {
-  /* const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
   const portfolios = api.portfolio.list.useQuery({
     page: 1,
-  }); */
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center py-12">
@@ -88,12 +71,12 @@ export default function Dashboard() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {portfolios.map((portfolio) => (
+                {portfolios.data?.map((portfolio) => (
                   <TableRow key={portfolio.id}>
                     <TableCell className="font-medium">
                       {portfolio.name}
                     </TableCell>
-                    <TableCell>{portfolio.dailyChange}</TableCell>
+                    <TableCell>{portfolio.dailyChangeAmount}</TableCell>
                     <TableCell>{portfolio.cashBalance}</TableCell>
                     <TableCell>{portfolio.totalValue}</TableCell>
 
